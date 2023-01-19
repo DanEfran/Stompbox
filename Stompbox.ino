3a82e9e477046422f248c8b31cd08167ab481153
Dan Efran <dev@efran.org>
2023-01-19 02:03:50 -0500

    Code cleanup and comments

--- Stompbox.ino
+++ Stompbox.ino
@@ -76,13 +76,14 @@ CRGB leds[NUM_LEDS];
 
 void startup_lightshow() {
 
+  // begin dark
   leds[0] = CHSV(0, 255, 0);
   for (int i = 1; i < NUM_LEDS; i++) {
     leds[i] = CHSV(50, 200, 0);
   }
   FastLED.show();
 
-  const long kk = 100;
+  // each lamp glows on
   for (int i = 1; i < NUM_LEDS; i++) {
     for (int j = 0; j < 128; j += 1) {
       leds[i] = CHSV(50, 200, j);
@@ -91,6 +92,7 @@ void startup_lightshow() {
     }
   }
 
+  // record light glows quite bright...
   for (int m = 0; m < 256; m += 1) {
     leds[0] = CHSV(0, 255, m);
     FastLED.show();
@@ -99,6 +101,7 @@ void startup_lightshow() {
 
   delay(500);
 
+  // ...then dims to idle
   for (int m = 255; m > 127; m -= 1) {
     leds[0] = CHSV(0, 255, m);
     FastLED.show();
@@ -107,6 +110,7 @@ void startup_lightshow() {
 
   delay(500);
 
+  // lamps each sparkle in antici...
   for (int i = 1; i < NUM_LEDS; i++) {
     int j;
     for (j = 127; j < 256; j += 8) {
@@ -114,17 +118,15 @@ void startup_lightshow() {
       FastLED.show();
       delay(1);
     }
-    
     delay(30);
-
     for (; j > 127; j -= 8) {
       leds[i] = CHSV(50, 200, j);
       FastLED.show();
       delay(1);
     }
-
     delay(15);
   }
+  // ...pation
 
 }
 
