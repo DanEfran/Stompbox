2792262d591a8f49d121c0d9e8a4174f704dc7f9
Dan Efran <dev@efran.org>
2023-01-18 22:26:21 -0500

    Head comment documenting arduino device type and stompbox-specific pinout.

--- Stompbox.ino
+++ Stompbox.ino
@@ -1,12 +1,70 @@
+/*
+
+Stompbox.ino
+Dan Efran, 2023
+
+A multiple foot switch control surface interfacing via OSC over USB.
+The hardware UI has a row of six foot-convenient buttons, 
+six corresponding LEDs, 
+three 30-step rotary encoder knobs, 
+a small analog joystick (acting as a crude expression pedal,
+and mounted diagonally to allow semi-separate control of two channels),
+and a standard 1/4" expression pedal input.
+There is also a small reset button, hardwired to reset the arduino.
+
+
+Arduino Device: Adafruit ItsyBitsy 32u4
+Pinout:
+0 N/C (reserved for USB serial)
+1 N/C (reserved for USB serial)
+2 Button 1
+3 Button 2
+4 Knob 1 select button
+5 Button 3
+6 Knob 2 select button
+7 Button 4
+8 Knob 3 select button
+9 Button 5
+10 Rotary 1 A
+11 Rotary 1 B
+12 Rotary 2 A
+13 N/C (LED_BUILTIN)
+14 (MISO) LED Data
+15 (SCK) Joystick select button
+16 (MOSI) Rotary 2 B
+A0 Joystick analog X
+A1 Joystick analog Y
+A2 External Pedal analog Z
+A3 Button 0 (record button)
+A4 Rotary 3 B
+A5 Rotary 3 A
+RST (Reset button is connected to reset pin.)
+
+*/
+
+
 #include <FastLED.h>
+
+#define SERIAL_LOGGING
 
 #define NUM_LEDS 6
 #define DATA_PIN 14
 CRGB leds[NUM_LEDS];
 
+void log(const char *) {
+
+#ifdef SERIAL_LOGGING
+  Serial.println("Stompbox looping.");
+#endif
+
+}
+
 void setup() {
 
+#ifdef SERIAL_LOGGING
   Serial.begin(57600);
+#endif
+
 	FastLED.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS);
 	FastLED.setBrightness(80);
 
@@ -25,6 +83,6 @@ void loop() {
   delay(200);
   leds[4] = CHSV(66, 100, 211);
   FastLED.show();
-  Serial.println("Stompbox looping.");
+  log("Stompbox looping.");
   delay(1700);
  }
