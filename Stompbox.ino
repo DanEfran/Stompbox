f7fbd3d6ffc1fa539cce32de36d4e3f79eba1e11
Dan Efran <dev@efran.org>
2023-01-18 19:42:35 -0500

    Basic lamp test/FastLED hello world: Record button is red, other lights are pale; one "flickers" yellow.

--- Stompbox.ino
+++ Stompbox.ino
@@ -1,9 +1,31 @@
+#include <FastLED.h>
+
+#define NUM_LEDS 6
+#define DATA_PIN 14
+CRGB leds[NUM_LEDS];
+
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(57600);
+	Serial.println("Stompbox resetting");
+
+	FastLED.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS);
+	FastLED.setBrightness(80);
+
+    leds[0] = CHSV(0, 255, 255);
+  	for(int i = 1; i < NUM_LEDS; i++) {
+		  leds[i] = CHSV(66, 100, 211);
+    }
+  FastLED.show();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-}
+  leds[4] = CHSV(66, 255, 255);
+  FastLED.show();
+  delay(200);
+  leds[4] = CHSV(66, 100, 211);
+  FastLED.show();
+  delay(1700);
+ }
